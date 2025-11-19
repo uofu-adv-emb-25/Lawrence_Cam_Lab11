030ae8a162951e7a11830b31daa1e58d5d794613
CaptainButter8 <tardis241@gmail.com>
2025-11-19 13:10:58 -0700

    Still working on temp

--- server/gatt_counter.c
+++ server/gatt_counter.c
@@ -55,6 +55,7 @@
 #include "gatt_counter.h"
 #include "btstack.h"
 #include "ble/gatt-service/battery_service_server.h"
+#include "temp_sense.h"
 
 #define HEARTBEAT_PERIOD_MS 1000
 
@@ -221,6 +222,9 @@ static uint16_t att_read_callback(hci_con_handle_t connection_handle, uint16_t a
     if (att_handle == ATT_CHARACTERISTIC_0000FF11_0000_1000_8000_00805F9B34FB_01_VALUE_HANDLE){
         return att_read_callback_handle_blob((const uint8_t *)counter_string, counter_string_len, offset, buffer, buffer_size);
     }
+    else if (att_handle == ATT_CHARACTERISTIC_ORG_BLUETOOTH_CHARACTERISTIC_TEMPERATURE_01_VALUE_HANDLE){
+        return (uint16_t)(temperature_poll()*100);
+    }
     return 0;
 }
 /* LISTING_END */
@@ -259,6 +263,7 @@ int btstack_main(void);
 int btstack_main(void)
 {
     le_counter_setup();
+    temperature_setup();
 
     // turn on!
 	hci_power_control(HCI_POWER_ON);
